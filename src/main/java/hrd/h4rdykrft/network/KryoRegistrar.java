076c5db323f01b145daca5cc80f9da4063fa5cd0
H4RDyAwp <hardyawpmaster@gmail.com>
2026-06-29 21:02:32 +0300

    Multiplayer FIXED

--- src/main/java/hrd/h4rdykrft/network/KryoRegistrar.java
+++ src/main/java/hrd/h4rdykrft/network/KryoRegistrar.java
@@ -8,8 +8,13 @@ public final class KryoRegistrar {
     }
 
     public static void register(Kryo kryo) {
+        // 1. Register ArrayList to fix the JoinResponse crash
         kryo.register(java.util.ArrayList.class);
+
+        // 2. Register PlayerInfo (because it lives inside the ArrayList)
         kryo.register(NetworkMessages.PlayerInfo.class);
+
+        // 3. Register the rest of your network messages in order
         kryo.register(NetworkMessages.JoinRequest.class);
         kryo.register(NetworkMessages.JoinResponse.class);
         kryo.register(NetworkMessages.PlayerJoined.class);
